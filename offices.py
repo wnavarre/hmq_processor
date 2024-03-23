a0561dea87744d8ae9f6a74a9cee652b9aad9514
William Navarre <git@navarre.xyz>
2024-03-22 23:56:13 -0400

    Offices metadata.

---
+++ offices.py
@@ -0,0 +1,7 @@
+rank = { "Mayor" : 10,
+         "City Council": 20,
+         "School Committee": 30 }
+
+adjective = { "Mayor" : "Mayoral",
+              "City Council": "City Council",
+              "School Committee": "School Committee" }
